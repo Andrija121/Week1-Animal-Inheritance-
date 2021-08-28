bb65dbc9a7bdd02e19c3fb7fbf01fba8308671c2
Andrija Hanga <a.hanga@student.fontys.nl>
2021-08-28 20:57:17 +0200

    Week1 Inheritance-Animal excersise

---
+++ Animal (Inheritance)/Animal.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal__Inheritance_
+{
+    class Animal
+    {
+        protected string name;
+        protected int age;
+
+        public Animal(string name)
+        {
+            this.name = name;
+            age = 1;
+        }
+        public Animal(string name,int age)
+        {
+            this.name = name;
+            if (age == 0)
+                age = 1;
+            else
+            this.age = age;
+        }
+        public void CelebrateBirthday()
+        {
+            if (age < 100)
+                age++;
+        }
+        public virtual string ToString()
+        {
+            return name + ( age + " year ");
+        }
+    }
+
+    class Cat:Animal
+    {
+        public Cat(string name):base(name)
+        {
+            age = 1;
+        }
+        public Cat(string name,int age) : base(name,age)
+        {
+
+        }
+        public void CelebrateBirthday()
+        {
+            if (age < 25)
+                age++;
+        }
+        public override string ToString()
+        {
+            return "Cat: " + base.ToString();
+        }
+
+    }
+
+    class Dog  :Animal
+    {
+        public Dog(string name) : base(name)
+        {
+            age = 1;
+        }
+        public Dog(string name, int age) : base(name, age)
+        {
+
+        }
+        public void CelebrateBirthday()
+        {
+            if (age < 20)
+                age++;
+        }
+        public override string ToString()
+        {
+            return "Dog: "+ base.ToString();
+        }
+
+    }
+
+    class Turtle :Animal
+    {
+        public Turtle(string name) : base(name)
+        {
+            age = 1;
+        }
+        public Turtle(string name, int age) : base(name, age)
+        {
+
+        }
+        public override string ToString()
+        {
+            return "Turtle: " + base.ToString();
+        }
+    }
+}
---
+++ Animal (Inheritance)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Animal__Inheritance_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Garfield ", 20);
+            Dog dog = new Dog("Lesie ", 30);
+            Turtle turtle = new Turtle("niki ");
+
+            cat.CelebrateBirthday();
+            dog.CelebrateBirthday();
+            
+            Console.WriteLine(cat.ToString());
+            Console.ReadLine();
+            Console.WriteLine(dog.ToString());
+            Console.ReadLine();
+            Console.WriteLine(turtle.ToString());
+            Console.ReadLine();
+        }
+    }
+}
